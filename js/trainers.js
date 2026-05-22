80dc9c9e3380a5f29aeb0735e1203c23ea0ce39a
SamiNiko <samuel04niko@gmail.com>
2026-05-22 23:20:05 +0200

    trainers.js reattivo a onSave + bump versioni per forzare aggiornamento PWA

--- js/trainers.js
+++ js/trainers.js
@@ -6,7 +6,7 @@
 
 import { loadAllPokemon, findPokemon } from './data/pokeapi.js';
 import { TRAINERS, getTrainer, getUnlockedTrainers, getTrainerTotalStars, getTrainerDifficulty } from './data/trainers.js?v=8';
-import { getTrainersBeaten, isTrainerBeaten } from './data/state.js?v=6';
+import { getTrainersBeaten, isTrainerBeaten, onSave } from './data/state.js?v=6';
 import { getRarity, tierStars }               from './data/rarity.js';
 import { playBGM }                            from './data/bgm.js?v=9';
 
@@ -23,6 +23,14 @@ let unlocked = [];
   renderList();
   renderProgress();
   bindModalCloseHandlers();
+
+  // Reattivo: se cloud-sync pulla in ritardo (con trainer appena battuti),
+  // la lista si aggiorna senza richiedere ricarica manuale.
+  onSave(() => {
+    refreshUnlocked();
+    renderList();
+    renderProgress();
+  });
 })();
 
 function refreshUnlocked() {
